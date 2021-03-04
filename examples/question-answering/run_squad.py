d19ddb2fc3624528e45960069e4696a3c15fac61
Dhiraj D. Kalamkar <dhiraj.d.kalamkar@intel.com>
2021-03-03 23:12:59 -0800

    Set rnd_seed for pcl_bert

--- examples/question-answering/run_squad.py
+++ examples/question-answering/run_squad.py
@@ -73,6 +73,7 @@ def set_seed(args):
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
+    if args.use_pcl: pcl_bert.set_rnd_seed(args.seed)
     if args.n_gpu > 0:
         torch.cuda.manual_seed_all(args.seed)
 
